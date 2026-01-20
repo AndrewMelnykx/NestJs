660ad276078f5ba6e000a39dc1506cccd2362915
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-20 13:33:33 +0100

    Added get requests

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+  @Get('all')
+  findAll() {
+    return this.taskService.findAll();
+  }
+  @Get('by-id/:id')
+  findById(@Param('id') id: string) {
+    return this.taskService.findById(Number(id));
+  }
+  @Post()
+  create() {
+    return this.taskService.create();
+  }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  private tasks = [
+    {
+      id: 1,
+      title: 'Learn NextJs',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: 'Build API',
+      isCompleted: true,
+    },
+  ];
+  findAll() {
+    return this.tasks;
+  }
+  findById(id: number) {
+    const task = this.tasks.find((task) => task.id === id);
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+    return task;
+  }
+  create() {
+    const newTask = {
+      id: this.tasks.length + 1,
+      title: 'New Task',
+      isCompleted: false,
+    };
+    this.tasks.push(newTask);
+    return this.tasks;
+  }
+}
