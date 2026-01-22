0a224749bf52730c8a40e775d53ea7e934136b58
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-22 13:17:56 +0100

    Added global decorator for user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-
+// import { MovieModule } from 'src/movie/movie.module';
+@Global()
 @Module({
+  // imports: [MovieModule],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
