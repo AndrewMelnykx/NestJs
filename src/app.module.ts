f162c747c65469f145ce09998028be6f5b7a0dd6
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-22 11:14:58 +0100

    New entity user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { MovieModule } from './movie/movie.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [TaskModule, MovieModule],
+  imports: [TaskModule, MovieModule, UserModule],
 })
 export class AppModule {}
