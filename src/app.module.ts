b08b3b59be176d8218c2b04ee0019b4c6e1aabbc
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-23 08:55:33 +0100

    Type orm added inside app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,24 @@ import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { MovieModule } from './movie/movie.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TaskModule,
+    MovieModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      password: '123456',
+      database: 'nestjs-course',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
-  imports: [TaskModule, MovieModule, UserModule],
 })
 export class AppModule {}
