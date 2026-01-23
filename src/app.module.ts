a491f396eeeec5fbca6fc043016aa0894b3a6386
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-23 12:48:30 +0100

    Config service added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,21 +5,29 @@ import { TaskModule } from './task/task.module';
 import { MovieModule } from './movie/movie.module';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TaskModule,
     MovieModule,
     UserModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      username: 'root',
-      port: 5433,
-      password: '123456',
-      database: 'nestjs-course',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('POSTGRES_HOST'),
+        username: 'root',
+        port: 5433,
+        password: '123456',
+        database: 'nestjs-course',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
