befeef62ded794d48a34baf4baa4bf10d1b13387
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-24 10:17:09 +0100

    Added movie entity

---
+++ src/movie/enteties/movie.entety.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+// { name: 'movies' }
+export class MovieEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  releasedYear: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
